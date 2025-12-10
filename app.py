d59e59096b9485ec7180489d023e00c394ae4849
vutheduyvl <vtduy050595i@tuyenquang.edu.vn>
2025-12-10 10:50:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Vũ Thế Duy - Trường Tiểu học Vĩnh Lợi - ĐT: 098 9849 628</p>
 </div>
 """, unsafe_allow_html=True)
 
